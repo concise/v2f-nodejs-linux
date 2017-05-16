655191b028a0e0a7d3cb731bc7b88fcbb7d1afef
JS <js@jong.sh>
2017-05-16 22:20:54 +0800

    update button-ruten-u2f-166905.js

--- button-ruten-u2f-166905.js
+++ button-ruten-u2f-166905.js
@@ -5,13 +5,13 @@ const URL_B = 'https://ruten-u2f-166905.appspot.com/api/req/';
 
 const wait = (ms, val) => new Promise(f => setTimeout(f, ms, val));
 
-const createApprovalRequest = () => (
+const createUserApprovalRequest = () => (
     fetch(URL_A, {method: 'post'})  // send POST request
         .then(s => s.text())        // parse the response as plain text
         .then(t => t.trimRight())   // remove trailing new line character
 );
 
-const getApprovalRequestState = (id) => (
+const getUserApprovalRequestState = (id) => (
     fetch(URL_B + id)               // send GET request
         .then(s => s.text())        // parse the response as plain text
         .then(t => t.trimRight())   // remove trailing new line character
@@ -19,7 +19,7 @@ const getApprovalRequestState = (id) => (
 
 const getUserApprovalBit = async () => {
 
-    const id = await createApprovalRequest();
+    const id = await createUserApprovalRequest();
 
     console.log('Somebody has to do' +
         ' `curl https://ruten-u2f-166905.appspot.com/api/action' +
@@ -33,7 +33,7 @@ const getUserApprovalBit = async () => {
 
     while (true) {
         const [_, state] = await Promise.all([
-            wait(1000), getApprovalRequestState(id) ]);
+            wait(1000), getUserApprovalRequestState(id) ]);
         if (state === 'accept')
             return true;
         if (state === 'reject')
